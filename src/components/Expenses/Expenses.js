4d8930935ae35a2805fb68d6a347bd2b85e650c6
dulecux <dusan.culibrk@live.com>
2021-12-01 00:10:22 +0100

    Expense state and input events finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter';
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {props.items.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} key={item.id} />
       ))}
     </Card>
   );
